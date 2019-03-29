9031add8936708dc9a6bf888240cc29bfb5fe9d3
Ginger Williams <gingerwilliams@MacBook-Pro.local>
2019-03-29 14:03:02 -0400

    dotnet hello world

---
+++ console/Class1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    public class Class1
+    {
+        public string ReturnMessage()
+        {
+            return "Happy Coding";
+        }
+    }
+}
---
+++ console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 c1 = new Class1();
+            Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+        }
+    }
+}
