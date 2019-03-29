7a5068324f948d5abb117efac9ce9ada85fa14e3
Ginger Williams <gingerwilliams@MacBook-Pro.local>
2019-03-29 16:09:46 -0400

    created a public method

--- golden/library/Thing.cs
+++ golden/library/Thing.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using static Newtonsoft.Json.JsonConvert;
 
 namespace library
 {
-    public class Class1
+    public class Thing
     {
+        //public method
+        public int Get(int a, int b) =>
+            DeserializeObject<int>($"{a + b}");
     }
 }
+
+// The `Thing` class contains one public method, `Get`, 
+// which returns the sum of two numbers but does so by 
+// converting the sum into a string and then deserializing 
+// it into an integer. 
+
+// This makes use of a number of modern C# features, 
+// such as using static directives, 
+// expression-bodied members, and string interpolation.
